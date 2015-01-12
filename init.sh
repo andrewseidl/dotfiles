871e9e1107d5f23b316adeaed600c4e5441e50fe
Andrew Seidl <dev@aas.io>
2015-01-12 05:00:53 -0600

    Rewrite init script; remove homeshick

--- init.sh
+++ init.sh
@@ -1,17 +1,43 @@
 #!/bin/sh
 
-if hash git 2>/dev/null ; then
-    
-    [[ ! -e $HOME/.homesick/repos/homeshick ]] && git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-    source $HOME/.homesick/repos/homeshick/homeshick.sh
+DFDIR="$HOME/.dotfiles"
 
-    homeshick clone andrewseidl/dotfiles
-
-    # tell Vundle to install plugins
-    vim +PluginInstall +qall
-
-    # install syntax highlighting for zsh
-    [[ ! -e $HOME/.zsh/plugins/zsh-syntax-highlighting ]] && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/plugins/zsh-syntax-highlighting
-else
+# check if git is installed
+if !hash git 2>/dev/null ; then
     echo "Please install Git and then rerun."
 fi
+
+# install syntax highlighting for zsh
+ZSHL=$HOME/.zsh/plugins/zsh-syntax-highlighting
+if [[ ! -e $ZSHL ]] ; then
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSHL
+fi
+
+# clone dotfiles if they don't exist
+if [[ ! -e "$DFDIR" ]] ; then
+    git clone git://github.com/andrewseidl/dotfiles.git "$DFDIR"
+fi
+
+# enter dotfiles dir
+pushd $PWD >/dev/null
+cd "$DFDIR"
+
+# pull updates if there are no outstanding changes
+if git diff --quiet ; then
+    git pull --quiet
+else
+    git status -s
+fi
+
+# for each dotfile, back up existing in $HOME and symlink new 
+for i in $( find $PWD/home -maxdepth 1 -mindepth 1 ); do
+    HOMEFILE="$HOME/$( basename $i )"
+    if [[ -e $HOMEFILE ]] ; then
+        if [[ "$i" != "$( readlink $HOMEFILE)" ]] ; then
+            mv $HOMEFILE{,-bak.$(date +%Y%m%d)}
+        fi
+    fi
+    ln -sf $i $HOME/
+done
+
+popd >/dev/null
