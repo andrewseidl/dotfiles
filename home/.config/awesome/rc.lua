c9af16a023ff45e1804c33225cc48af390c4856e
Andrew Seidl <dev@aas.io>
2019-12-05 10:40:14 -0800

    awesome/xinit: add screen lock shortcut

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -314,6 +314,8 @@ globalkeys = gears.table.join(
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
+    awful.key({ modkey, "Control" }, "Delete", function () awful.util.spawn("xscreensaver-command -lock") end,
+              {description = "lock screen"}),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
               {description = "increase master width factor", group = "layout"}),
