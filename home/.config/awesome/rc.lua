2ac3269ded9eb7cdf18a5a7a6490e7d36744f9ca
Andrew Seidl <dev@aas.io>
2020-03-28 09:13:14 -0700

    awesome: autostart nm-applet

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -599,3 +599,5 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+awful.spawn.single_instance("nm-applet")
